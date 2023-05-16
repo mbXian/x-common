c54185aaa16c7a9263cc3fcb0f2e1d7bc144f240
Ben <18824140606@163.com>
2023-05-16 16:18:52 +0800

    update: CommonConstant

---
+++ x-common-core/src/main/java/com/xmb/common/constant/CommonConstant.java
@@ -0,0 +1,80 @@
+package com.xmb.common.constant;
+
+public interface CommonConstant {
+
+    /**
+     * 系统用户id
+     */
+    public static final String SYSTEM_USER_ID = "system";
+
+    /**
+     * 分隔线
+     */
+    public static final String DIVIDE_LINE = "---------------";
+
+	/**
+	 * 正常状态
+	 */
+	public static final Integer STATUS_NORMAL = 0;
+
+	/**
+	 * 禁用状态
+	 */
+	public static final Integer STATUS_DISABLE = -1;
+
+	/**
+	 * 删除标志
+	 */
+	public static final Integer DEL_FLAG_1 = 1;
+
+	/**
+	 * 未删除
+	 */
+	public static final Integer DEL_FLAG_0 = 0;
+
+    /**
+     * 逗号_字符
+     */
+    public static final char COMMA_CHAR = ',';
+    /**
+     * 逗号
+     */
+    public static final String COMMA = ",";
+    /**
+     * 分号
+     */
+    public static final String SEMICOLON = ";";
+    /**
+     * 竖杠号
+     */
+    public static final String VERTICAL_BAR = "|";
+    /**
+     * 斜杠号
+     */
+    public static final String TRAILING_SLASH = "/";
+    /**
+     * 点号（转义）
+     */
+    public static final String TRANSFERRED_DOT = "\\.";
+    /**
+     * 点号
+     */
+    public static final String DOT = ".";
+    /**
+     * 下划线
+     */
+    public static final String UNDERLINE = "_";
+    /**
+     * 中划线
+     */
+    public static final String MID_LINE = "-";
+    /**
+     * 冒号
+     */
+    public static final String COLON = ":";
+    /**
+     * 空格
+     */
+    public static final String SPACE = " ";
+
+}
